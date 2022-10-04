b002b32b44f7d169943f1a8ff86beb9e06fc4715
Gagan Kahal <73100470+gaganjot99@users.noreply.github.com>
2022-10-04 11:01:13 +0530

    feat: complete functional tests

--- boilerplate-project-metricimpconverter/tests/1_unit-tests.js
+++ boilerplate-project-metricimpconverter/tests/1_unit-tests.js
@@ -5,25 +5,46 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
-   test('ConvertHandler can read any type of number', ()=>{
-    assert.equal(convertHandler.getNum('45mi'), 45, 'getNum is not working with whole numbers');
-    assert.equal(convertHandler.getNum('4.5mi'), 4.5, 'getNum is not working with decimal numbers');
-    assert.equal(convertHandler.getNum('4/5mi'), 4/5, 'getNum is not working with fractions numbers');
-    assert.equal(convertHandler.getNum('4/5.5mi'), 4/5.5, 'getNum is not working with fractional decimal numbers');
+
+   test('convertHandler should correctly read a whole number input',()=>{
+      assert.equal(convertHandler.getNum('45mi'), 45, 'getNum is not working with whole numbers');
+
+   })
+   test('convertHandler should correctly read a decimal number input',()=>{
+      assert.equal(convertHandler.getNum('4.5mi'), 4.5, 'getNum is not working with decimal numbers');
+      
+   })
+   test('convertHandler should correctly read a fractional input',()=>{
+      assert.equal(convertHandler.getNum('4/5mi'), 4/5, 'getNum is not working with fractions numbers');
+      
+   })
+   test('convertHandler should correctly read a fractional input with a decimal',()=>{
+      assert.equal(convertHandler.getNum('4/5.5mi'), 4/5.5, 'getNum is not working with fractional decimal numbers');
+      
+   })
+   test('convertHandler should correctly return an error on a double-fraction', ()=>{
     assert.equal(convertHandler.getNum('4/5/7mi'), 'invalid number', 'getNum is not working with two / numbers');
-    assert.equal(convertHandler.getNum('mi'), 1, '1 is returned for no number mentioned');
+
+   })
+   test('convertHandler should correctly default to a numerical input of 1 when no numerical input is provided',()=>{
+      assert.equal(convertHandler.getNum('mi'), 1, '1 is returned for no number mentioned');
+      
    })
 
-   test('ConvertHandler can handle units', ()=>{
+   test('convertHandler should correctly read each valid input unit', ()=>{
     assert.equal(convertHandler.getUnit('45mi'), 'mi', 'getUnit is not working');
     assert.equal(convertHandler.getUnit('4.5kg'), 'kg', 'getUnit is not working');
     assert.equal(convertHandler.getUnit('4/5kM'), 'km', 'getUnit is not working');
     assert.equal(convertHandler.getUnit('4/5.5l'), 'L', 'getUnit is not working');
     assert.equal(convertHandler.getUnit('mI'), 'mi', 'getUnit is not working');
-    assert.equal(convertHandler.getUnit('46kvo'), 'invalid unit', 'getUnit is not working');
+   })
+   
+   test('convertHandler should correctly return an error for an invalid input unit',()=>{
+      assert.equal(convertHandler.getUnit('46kvo'), 'invalid unit', 'getUnit is not working');
+      
    })
 
-   test('ConvertHandler can return the correct return units', ()=>{
+   test('convertHandler should return the correct return unit for each valid input unit', ()=>{
     assert.equal(convertHandler.getReturnUnit('mi'), 'km');
     assert.equal(convertHandler.getReturnUnit('kg'), 'lbs');
     assert.equal(convertHandler.getReturnUnit('km'), 'mi');
@@ -32,7 +53,7 @@ suite('Unit Tests', function(){
     assert.equal(convertHandler.getReturnUnit('kvo'), 'invalid unit');
    })
 
-   test('ConvertHandler can can spell units', ()=>{
+   test('convertHandler should correctly return the spelled-out string unit for each valid input unit', ()=>{
       assert.equal(convertHandler.spellOutUnit('mi'), 'miles');
       assert.equal(convertHandler.spellOutUnit('kg'), 'kilograms');
       assert.equal(convertHandler.spellOutUnit('km'), 'kilometers');
@@ -41,15 +62,27 @@ suite('Unit Tests', function(){
       assert.equal(convertHandler.spellOutUnit('kvo'), 'invalid unit');
      })
 
-     test('ConvertHandler can return correct conversion results', ()=>{
-      assert.equal(convertHandler.convert(3.3,'mi'), '5.31082km');
-      assert.equal(convertHandler.convert(20,'lbs'), '9.07184kg');
+     test('convertHandler should correctly convert gal to L',()=>{
+      assert.equal(convertHandler.convert(4,'gal'), '15.14164L');
+   })
+   test('convertHandler should correctly convert L to gal',()=>{
       assert.equal(convertHandler.convert(13,'L'), '3.43424gal');
-     })
-
-     test('ConvertHandler can return correct final strings ', ()=>{
-      assert.equal(convertHandler.getString(3.3,'mi',5.31082,'km'), '3.3 miles converts to 5.31082 kilometers');
-      assert.equal(convertHandler.getString(13,'L',3.43424,'gal'), '13 liters converts to 3.43424 gallons');
-     })
+      
+   })
+   test('convertHandler should correctly convert mi to km', ()=>{
+      assert.equal(convertHandler.convert(3.3,'mi'), '5.31082km');
+   })
+   
+   test('convertHandler should correctly convert km to mi',()=>{
+      assert.equal(convertHandler.convert(132,'km'), '82.0212mi');
+   })
+   test('convertHandler should correctly convert lbs to kg',()=>{
+      assert.equal(convertHandler.convert(20,'lbs'), '9.07184kg');
+      
+   })
+   test('convertHandler should correctly convert kg to lbs',()=>{
+      assert.equal(convertHandler.convert(23,'kg'), '50.70636lbs');
+   })
+   
 
 });
--- boilerplate-project-metricimpconverter/tests/2_functional-tests.js
+++ boilerplate-project-metricimpconverter/tests/2_functional-tests.js
@@ -6,5 +6,100 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    const responses = {
+        "12L":{"initNum":12,"initUnit":"L","returnNum":3.17007,"returnUnit":"gal","string":"12 liters converts to 3.17007 gallons"},
+        "32g": "invalid unit",
+        "3/7.2/4kg": "invalid number",
+        "3/7.2/4kilomegagram": "invalid number and unit",
+        "kg": {"initNum":1,"initUnit":"kg","returnNum":2.20462,"returnUnit":"lbs","string":"1 kilograms converts to 2.20462 pounds"}
+    }
+
+    this.timeout(5000)
+    test('Convert a valid input such as 12L: GET request to /api/convert',function(done){
+    let input = '12L'
+    chai
+    .request(server)
+    .get(`/api/convert?input=${input}`)
+    .end(function(err, doc){
+        if(err){
+            console.log(err)
+            done()
+        }
+        assert.equal(doc.status, 200);
+        assert.deepEqual(doc.body,responses[input])
+        done()
+    })
+   })
+
+   test('Convert an invalid input such as 32g: GET request to /api/convert',(done)=>{
+    
+    let input = '32g'
+    chai
+    .request(server)
+    .get(`/api/convert?input=${input}`)
+    .end((err, doc)=>{
+        if(err){
+            console.log(err)
+            done()
+        }
+        assert.equal(doc.status, 200);
+        assert.deepEqual(doc.text,responses[input])
+        done()
+    })
+   })
+
+   test('Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert',(done)=>{
+    
+    let input = '3/7.2/4kg'
+    chai
+    .request(server)
+    .get(`/api/convert?input=${input}`)
+    .end((err, doc)=>{
+        if(err){
+            console.log(err)
+            done()
+        }
+        assert.equal(doc.status, 200);
+        assert.deepEqual(doc.text,responses[input])
+        done()
+    })
+   })
+
+   test('Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert',(done)=>{
+    
+    let input = '3/7.2/4kilomegagram'
+    chai
+    .request(server)
+    .get(`/api/convert?input=${input}`)
+    .end((err, doc)=>{
+        if(err){
+            console.log(err)
+            done()
+        }
+        assert.equal(doc.status, 200);
+        assert.deepEqual(doc.text,responses[input])
+        done()
+    })
+   })
+
+   test('Convert with no number such as kg: GET request to /api/convert',(done)=>{
+    
+    let input = 'kg'
+    chai
+    .request(server)
+    .get(`/api/convert?input=${input}`)
+    .end((err, doc)=>{
+        if(err){
+            console.log(err)
+            done()
+        }
+        assert.equal(doc.status, 200);
+        assert.deepEqual(doc.body,responses[input])
+        done()
+    })
+   })
+
+   
+
 
 });
