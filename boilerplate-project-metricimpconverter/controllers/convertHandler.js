9b059614f35c8c21afaa978b5cc6c492f257c17f
Gagan Kahal <73100470+gaganjot99@users.noreply.github.com>
2022-10-04 08:05:26 +0530

    feat: complete handlers and their units tests

--- boilerplate-project-metricimpconverter/controllers/convertHandler.js
+++ boilerplate-project-metricimpconverter/controllers/convertHandler.js
@@ -1,42 +1,89 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
+    if(input.includes('/')){
+      if(input.split('/').length > 2){
+        return 'not valid number'
+      }
+    }
+    let result = eval(input.split(/[a-z]+/)[0])
+    if(result == undefined){
+      return 1;
+    }
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
-    
+    let result = input.match(/[a-zA-Z]+$/)[0]
+    if(!['mi','km','L','gal','lbs','kg'].includes(result)){
+      return 'not valid unit'
+    }
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    switch (initUnit){
+      case 'mi':
+        return 'km'
+      case 'km':
+        return 'mi'
+      case 'kg':
+        return 'lbs'
+      case 'lbs':
+        return 'kg'
+      case 'L':
+        return 'gal'
+      case 'gal':
+        return 'L'
+      default:
+        return 'not valid unit'
+    }
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    switch (unit){
+      case 'mi':
+        return 'miles'
+      case 'km':
+        return 'kilometers'
+      case 'kg':
+        return 'kilograms'
+      case 'lbs':
+        return 'pounds'
+      case 'L':
+        return 'liters'
+      case 'gal':
+        return 'gallons'
+      default:
+        return 'not valid unit'
+    }
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    switch (initUnit){
+      case 'mi':
+        return parseFloat((initNum*miToKm).toFixed(5))+'km'
+      case 'km':
+        return parseFloat((initNum/miToKm).toFixed(5))+'mi'
+      case 'kg':
+        return parseFloat((initNum/lbsToKg).toFixed(5))+'lbs'
+      case 'lbs':
+        return parseFloat((initNum*lbsToKg).toFixed(5))+'kg'
+      case 'L':
+        return parseFloat((initNum/galToL).toFixed(5))+'gal'
+      case 'gal':
+        return parseFloat((initNum*galToL).toFixed(5))+'L'
+      default:
+        return 'not valid unit'
+    }
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
     
-    return result;
   };
   
 }
--- boilerplate-project-metricimpconverter/tests/1_unit-tests.js
+++ boilerplate-project-metricimpconverter/tests/1_unit-tests.js
@@ -5,5 +5,51 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+   test('ConvertHandler can read any type of number', ()=>{
+    assert.equal(convertHandler.getNum('45mi'), 45, 'getNum is not working with whole numbers');
+    assert.equal(convertHandler.getNum('4.5mi'), 4.5, 'getNum is not working with decimal numbers');
+    assert.equal(convertHandler.getNum('4/5mi'), 4/5, 'getNum is not working with fractions numbers');
+    assert.equal(convertHandler.getNum('4/5.5mi'), 4/5.5, 'getNum is not working with fractional decimal numbers');
+    assert.equal(convertHandler.getNum('4/5/7mi'), 'not valid number', 'getNum is not working with two / numbers');
+    assert.equal(convertHandler.getNum('mi'), 1, '1 is returned for no number mentioned');
+   })
+
+   test('ConvertHandler can handle units', ()=>{
+    assert.equal(convertHandler.getUnit('45mi'), 'mi', 'getUnit is not working');
+    assert.equal(convertHandler.getUnit('4.5kg'), 'kg', 'getUnit is not working');
+    assert.equal(convertHandler.getUnit('4/5km'), 'km', 'getUnit is not working');
+    assert.equal(convertHandler.getUnit('4/5.5L'), 'L', 'getUnit is not working');
+    assert.equal(convertHandler.getUnit('mi'), 'mi', 'getUnit is not working');
+    assert.equal(convertHandler.getUnit('46kvo'), 'not valid unit', 'getUnit is not working');
+   })
+
+   test('ConvertHandler can return the correct return units', ()=>{
+    assert.equal(convertHandler.getReturnUnit('mi'), 'km');
+    assert.equal(convertHandler.getReturnUnit('kg'), 'lbs');
+    assert.equal(convertHandler.getReturnUnit('km'), 'mi');
+    assert.equal(convertHandler.getReturnUnit('L'), 'gal');
+    assert.equal(convertHandler.getReturnUnit('gal'), 'L');
+    assert.equal(convertHandler.getReturnUnit('kvo'), 'not valid unit');
+   })
+
+   test('ConvertHandler can can spell units', ()=>{
+      assert.equal(convertHandler.spellOutUnit('mi'), 'miles');
+      assert.equal(convertHandler.spellOutUnit('kg'), 'kilograms');
+      assert.equal(convertHandler.spellOutUnit('km'), 'kilometers');
+      assert.equal(convertHandler.spellOutUnit('L'), 'liters');
+      assert.equal(convertHandler.spellOutUnit('gal'), 'gallons');
+      assert.equal(convertHandler.spellOutUnit('kvo'), 'not valid unit');
+     })
+
+     test('ConvertHandler can return correct conversion results', ()=>{
+      assert.equal(convertHandler.convert(3.3,'mi'), '5.31082km');
+      assert.equal(convertHandler.convert(20,'lbs'), '9.07184kg');
+      assert.equal(convertHandler.convert(13,'L'), '3.43424gal');
+     })
+
+     test('ConvertHandler can return correct final strings ', ()=>{
+      assert.equal(convertHandler.getString(3.3,'mi',5.31082,'km'), '3.3 miles converts to 5.31082 kilometers');
+      assert.equal(convertHandler.getString(13,'L',3.43424,'gal'), '13 liters converts to 3.43424 gallons');
+     })
 
 });
