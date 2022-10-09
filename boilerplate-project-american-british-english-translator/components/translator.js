3ca6685959d8f52e70eb5e2f39c3ae500a545a6f
Gagan Kahal <73100470+gaganjot99@users.noreply.github.com>
2022-10-09 18:03:39 +0530

    feat: complete the translator functions

--- boilerplate-project-american-british-english-translator/components/translator.js
+++ boilerplate-project-american-british-english-translator/components/translator.js
@@ -5,12 +5,73 @@ const britishOnly = require("./british-only.js");
 
 class Translator {
   americanToBritish(string) {
-    const stringArr = string.split(" ");
-    return stringArr.join("");
+    let stringArr = string;
+    for (let spell in americanOnly) {
+      let re = new RegExp(`${spell}([. ])`, "gi");
+      stringArr = stringArr.replace(
+        re,
+        `<span class="highlight">${americanOnly[spell]}</span>$1`
+      );
+    }
+    for (let spell in americanToBritishSpelling) {
+      let re = new RegExp(`${spell}([. ])`, "gi");
+      stringArr = stringArr.replace(
+        re,
+        `<span class="highlight">${americanToBritishSpelling[spell]}</span>$1`
+      );
+    }
+    for (let spell in americanToBritishTitles) {
+      let re = new RegExp(`${spell}([. ])`, "gi");
+      stringArr = stringArr.replace(
+        re,
+        `<span class="highlight">${americanToBritishTitles[spell]}</span>$1`
+      );
+    }
+    let newArr = stringArr.split(" ").map((element) => {
+      if (/^[0-9]+:[0-9]+$/.test(element)) {
+        return `<span class="highlight">${element.replace(":", ".")}</span>`;
+      }
+      return element;
+    });
+
+    return newArr.join(" ");
   }
   britishToAmerican(string) {
-    const stringArr = string.split(" ");
-    return stringArr.join("");
+    let stringArr = string;
+    for (let spell in britishOnly) {
+      let re = new RegExp(`${spell}([. ])`, "gi");
+      stringArr = stringArr.replace(
+        re,
+        `<span class="highlight">${britishOnly[spell]}</span>$1`
+      );
+    }
+    for (let spell in americanToBritishSpelling) {
+      let re = new RegExp(`${americanToBritishSpelling[spell]}([. ])`, "gi");
+      stringArr = stringArr.replace(
+        re,
+        `<span class="highlight">${spell}</span>$1`
+      );
+    }
+    for (let spell in americanToBritishTitles) {
+      let re = new RegExp(`${americanToBritishTitles[spell]}([. ])`, "gi");
+      stringArr = stringArr.replace(
+        re,
+        `<span class="highlight">${spell}</span>$1`
+      );
+    }
+    let newArr = stringArr.split(" ").map((element) => {
+      if (/^[0-9]+.[0-9]+[.]*$/.test(element)) {
+        if (element[element.length - 1] == ".") {
+          return `<span class="highlight">${element
+            .replace(".", ":")
+            .replace(".", "")}</span>.`;
+        }
+        return `<span class="highlight">${element.replace(".", ":")}</span>`;
+      }
+      return element;
+    });
+
+    return newArr.join(" ");
   }
 }
 
