592de2cdeeb3f996d95acd258ea596a893f2987c
Gagan Kahal <73100470+gaganjot99@users.noreply.github.com>
2022-10-09 16:38:58 +0530

    feat: add all unit tests

--- boilerplate-project-american-british-english-translator/components/translator.js
+++ boilerplate-project-american-british-english-translator/components/translator.js
@@ -1,10 +1,17 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
-
+  americanToBritish(string) {
+    const stringArr = string.split(" ");
+    return stringArr.join("");
+  }
+  britishToAmerican(string) {
+    const stringArr = string.split(" ");
+    return stringArr.join("");
+  }
 }
 
 module.exports = Translator;
